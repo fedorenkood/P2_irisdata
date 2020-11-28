7d5546b4157a7d6ba042ada860e06baf6874ad24
fedorenkood <fedorenkoalex23@gmail.com>
2020-11-28 15:14:06 -0600

    Set Up done, Question 1d completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import matplotlib.patches as mpatches
 
 
@@ -43,17 +44,18 @@ def classification(data, color_map):
 
 
 # this is used for 1c
-def line_calculation(length):
-    return -1/4 * length + 2.9
+def line_calculation(inputs, weights):
+    return -(inputs[0] * weights[0] + inputs[1] * weights[1])
 
 
 # inputs are of the form [width, length], weights are of the form [w0, w1, w2], and w0 is the weight for the bias node.
-def singleLayerOutput(inputs, weights):
-    sum = 1 * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2]
-    return logisticFct(sum)
+def single_layer_output(inputs, weights):
+    sum = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2]
+    # TODO: I multiplied it by 3 to make it look steeper
+    return logistic(sum * 3)
 
 
-def logisticFct(x):
+def logistic(x):
     return 1/(math.pow(math.e, -x) + 1)
 
 
@@ -66,10 +68,12 @@ def main():
     red_patch = mpatches.Patch(color='red', label='Setosa')
     blue_patch = mpatches.Patch(color='blue', label='Virginica')
     green_patch = mpatches.Patch(color='green', label='Versicolor')
+    weights = [-2.9, 0.25, 1]  # These separate the two important things nicely
 
     # plots for Q1
     # Q1a
-    f, (ax1, ax2) = plt.subplots(1, 2, sharex='none', sharey='none', figsize=(10, 5))
+    fig = plt.figure(figsize=(10, 8))
+    ax1 = fig.add_subplot(221)
     ax1.scatter(iris_objects['green'].petal_lengths, iris_objects['green'].petal_widths, c='green', alpha=0.5)
     ax1.scatter(iris_objects['blue'].petal_lengths, iris_objects['blue'].petal_widths, c='blue', alpha=0.5)
 
@@ -85,38 +89,38 @@ def main():
     line_lengths = np.linspace(1, 7.5, 100)
     line_widths = []
     for i in range(len(line_lengths)):
-        line_widths.append(line_calculation(line_lengths[i]))
+        line_widths.append(line_calculation([1, line_lengths[i]], weights))
 
     # Plot of line vs scatter
+    ax2 = fig.add_subplot(222, sharey=ax1, sharex=ax1)
     ax2.scatter(iris_objects['green'].petal_lengths, iris_objects['green'].petal_widths, c='green', alpha=0.5)
     ax2.scatter(iris_objects['blue'].petal_lengths, iris_objects['blue'].petal_widths, c='blue', alpha=0.5)
     ax2.plot(line_lengths, line_widths, 'r')
 
-    ax2.set_ylim([0.5, 3])
-    ax2.set_xlim([2.5, 7.5])
-    ax2.set_ylabel('Petal Width')
-    ax2.set_xlabel('Petal Length')
     ax2.legend(handles=[green_patch, blue_patch])
     ax2.grid(True)
 
+    # TODO: Q3d graph
+    ax3 = fig.add_subplot(223, projection='3d')
+
+    n = 100
+    graph_widths = np.linspace(0, 4, n)
+    graph_lengths = np.linspace(0, 7.5, n)
+    x_axis, y_axis = np.meshgrid(graph_lengths, graph_widths)
+    graph_outputs = []
+    for x in range(0, n):
+        graph_outputs.append([])
+        for y in range(0, n):
+            graph_outputs[x].append(single_layer_output([1, x_axis[x][y], y_axis[x][y]], weights))
+    graph_outputs = np.array(graph_outputs)
+
+    ax3.set_zlim([0, 1])
+    ax3.plot_surface(x_axis, y_axis, graph_outputs, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax3.set_ylabel('Petal Width')
+    ax3.set_xlabel('Petal Length')
+
     # Actually graphing it
     plt.show()
-
-    # now lets make a sicc 3d graph (work in progress)
-    weights = [2.9, -1, -1 / 4]  # These separate the two important things nicely
-
-
-    '''
-    n = 100
-    graphWidths = np.linspace(0, 2.6, n)
-    graphLengths = np.linspace(1, 7, n)
-    X, Y = np.meshgrid(graphWidths, graphLengths)
-    graphOutputs = []
-    print(len(inputMesh[0]), "is the input mesh length")
-    for i in range(len(inputMesh[0])):
-        input = [inputMesh]
-        graphOutputs.append(singleLayerOutput(inputMesh, weights))
-    '''
 
 
 
